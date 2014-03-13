739dc4b8d70624f7e0127a990209a55c35297679
Mark Seemann <mark@ploeh.dk>
2014-03-13 08:49:54 +0100

    Added some code to pull a message off a queue.
    Manual testing shows that it works well if you put a proper value in the
    connection string in App.config.

--- Src/ChainnelMail.CommandLine/Program.fs
+++ Src/ChainnelMail.CommandLine/Program.fs
@@ -1,3 +1,20 @@
-﻿[<EntryPoint>]
+﻿open Microsoft.WindowsAzure
+open Microsoft.WindowsAzure.Storage
+
+module AzureQ =
+    let dequeue (q : Queue.CloudQueue) =
+        match q.GetMessage() with
+        | null -> None
+        | msg -> Some(msg)
+
+[<EntryPoint>]
 let main argv = 
+    let storageAccount =
+        CloudConfigurationManager.GetSetting "storageConnectionString"
+        |> CloudStorageAccount.Parse
+
+    let q = storageAccount.CreateCloudQueueClient().GetQueueReference("qaiain")
+    q.CreateIfNotExists() |> ignore
+    
+    let msg = q |> AzureQ.dequeue
     0 // return an integer exit code
