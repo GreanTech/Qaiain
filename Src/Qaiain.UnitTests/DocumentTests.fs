07c4e18d0cf319d6c8f042793a0fc467b0c7cf71
Nikos Baxevanis <nikos.baxevanis@gmail.com>
2014-06-06 01:09:08 +0300

    Introduced Document module. This module should accept a message and create an Email or EmailReference from that message.

--- Src/Qaiain.UnitTests/DocumentTests.fs
+++ Src/Qaiain.UnitTests/DocumentTests.fs
@@ -1,1 +1,27 @@
-﻿namespace Grean.Qaiain.UnitTests
+﻿module Grean.Qaiain.UnitTests.DocumentTests
+
+open Grean.Qaiain
+open Xunit
+open Xunit.Extensions
+
+let verify = Swensen.Unquote.Assertions.test
+
+[<Theory>]
+[<InlineData("")>]
+[<InlineData("1234567890")>]
+[<InlineData("urn:foo:schemas:bar")>]
+[<InlineData("urn:bar:schemas:baz:1234")>]
+[<InlineData("urn:grean:schemas:email-reference:2011")>]
+[<InlineData("urn:grean:schemas:email-reference:2012")>]
+[<InlineData("urn:grean:schemas:email-reference:2013")>]
+[<InlineData("urn:grean:schemas:email-reference:2015")>]
+let ToDocumentTypeForUnknownNamespaceNameReturnsCorrectResult namespaceName =
+    let message =
+        @"<?xml version=""1.0""?>
+          <email-reference xmlns=""" + namespaceName + @""">
+            <email-data-address>http://blobs.foo.bar/baz/qux</email-data-address>
+          </email-reference>"
+
+    let actual = message |> ToDocumentType
+
+    verify <@ Unknown = actual @>
--- Src/Qaiain/Document.fs
+++ Src/Qaiain/Document.fs
@@ -6,3 +6,12 @@ open System.Xml.Linq
 type DocumentType =
     | Email of XDocument
     | EmailReference of XDocument
+    | Unknown
+
+[<AutoOpen>]
+module Document =
+
+    let ToDocumentType message =
+        let document = message |> XDocument.Parse
+        match document.Root.Name.Namespace.NamespaceName with
+        | _ -> Unknown
